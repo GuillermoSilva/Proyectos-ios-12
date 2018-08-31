431fc8ccc6831d5c69359808f7bf59e314a150df
GuillermoSilva <edisonsilvac@gmail.com>
2018-08-30 19:50:22 -0500

    Initial Commit

---
+++ 001-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  001-iamrich
+//
+//  Created by gsilvambook on 30/8/18.
+//  Copyright © 2018 gsilvambook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
