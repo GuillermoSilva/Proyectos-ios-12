39ee36102b582c73067e4fafdc8e433b6e3805df
GuillermoSilva <edisonsilvac@gmail.com>
2018-09-12 17:27:13 -0500

    Proyecto 001 I am Rich Gs

--- 001-iamrich/ViewController.swift
+++ 001-iamrich/ViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBOutlet weak var imageViewDiamond: UIImageView!
+    
+    
+    @IBOutlet weak var buutonPush: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //METODOS
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //self.labelTitle.text = "Haz Pulsado"
+        
+        //self.labelTitle.textColor = UIColor.green
+        
+        //self.labelTitle.font = UIFont.systemFont(ofSize: 50)
+        //self.imageViewDiamond.image = UIImage(named: "trompo-diamante")
+        
+        let controller = UIAlertController(title: "I am Rich", message: """
+                                                    I am Rich.
+                                                    I deserve it.
+                                                    I am good.
+                                                    Healthy and succedfull
+                                            """, preferredStyle: .actionSheet)
+        let action = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        
+        controller.addAction(action)
+        
+        let action2 = UIAlertAction(title: "Borrar", style: .destructive, handler: nil)
+        
+        controller.addAction(action2)
+        
+        let action3 = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        controller.addAction(action3)
+        
+        self.show(controller, sender: nil)
+        
+        
+    }
+    
+    
+    
+    
+    
 
 
 }
